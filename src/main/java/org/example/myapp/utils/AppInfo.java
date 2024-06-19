f33ab6d79cb3c3060405bb114e8b8138ee5dfa28
czsin <1425239022@qq.com>
2024-06-19 11:32:05 +0800

    新增了租用与报废页面

--- src/main/java/org/example/myapp/utils/AppInfo.java
+++ src/main/java/org/example/myapp/utils/AppInfo.java
@@ -3,7 +3,7 @@ package org.example.myapp.utils;
 public class AppInfo {
 
     //资源前缀
-    public static final String FileRootPath="C:\\Users\\75654\\Desktop\\软件工程\\MyApp\\pic\\";
+    public static final String FileRootPath="E:\\java_projects\\login\\demo\\pic\\";
 
     public static final String WebRootPath="http://localhost:8080/downloadImg/";
 }
